ae427176417bfb5f4031a220d09a5b626ace8996
KatarinaStjepanovic <katarina22april@gmail.com>
2024-09-17 20:52:20 +0200

    Removed .env folder

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -5,19 +5,37 @@
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  require('dotenv').config();
-
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-  };
+  
 
+/*const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWURFUNo0W2xHiuXsNVokbTi50-2LomH8",
+  authDomain: "katarina-portfolio.firebaseapp.com",
+  projectId: "katarina-portfolio",
+  storageBucket: "katarina-portfolio.appspot.com",
+  messagingSenderId: 1009796328455,
+  appId: "1:1009796328455:web:801ec752be5d8d04438ea2",
+  databaseURL: "https://katarina-portfolio-default-rtdb.europe-west1.firebasedatabase.app"
+};
+
+/*FIREBASE_API_KEY=AIzaSyAWURFUNo0W2xHiuXsNVokbTi50-2LomH8
+FIREBASE_AUTH_DOMAIN=katarina-portfolio.firebaseapp.com
+FIREBASE_PROJECT_ID=katarina-portfolio
+FIREBASE_STORAGE_BUCKET=katarina-portfolio.appspot.com
+FIREBASE_MESSAGING_SENDER_ID=1009796328455
+FIREBASE_APP_ID=1:1009796328455:web:801ec752be5d8d04438ea2
+FIREBASE_DATABASE_URL=https://katarina-portfolio-default-rtdb.europe-west1.firebasedatabase.app
+*/
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
